7645f56a34f2b4438c236a4fd09eb22f6bd063ba
mixedsider <dmstjd0812@gmail.com>
2025-02-04 13:05:28 +0900

    feat(lv.5): 예외 발생 처리
    응답에 따른 예외 처리 추가

---
+++ src/main/java/com/example/scheduleproject/entity/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.example.scheduleproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
